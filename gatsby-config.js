6a025240d20d36e6afeae14312b86ab4eb950b03
vishnuramv <vishnu.v1902@gmail.com>
2020-11-18 18:15:08 +0530

    Added Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/undraw_under_construction_46pa.svg`, // This path is relative to the root of the site.
+        icon: `src/images/favicon_io/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
