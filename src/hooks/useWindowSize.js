0e185be90c6b82f22c1f2590ce2a901f0b9e60b5
vishnuramv <vishnu.v1902@gmail.com>
2020-12-04 10:53:36 +0530

    Completed Version1

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react';
 
 const useWindowSize = () => {
-    const getSize = () => {
-        return {
-            width: (typeof window !== `undefined`) ? window.innerWidth : 1366,
-            height: (typeof window !== `undefined`) ? window.innerHeight : null,
-        }
-    }
-    const [windowSize, SetWindowSize] = useState(getSize);
+    // const getSize = () => {
+    //     return {
+    //         width: (typeof window !== `undefined`) ? window.innerWidth : null,
+    //         height: (typeof window !== `undefined`) ? window.innerHeight : null,
+    //     }
+    // }
+    const [windowSize, SetWindowSize] = useState({
+        width: (typeof window !== `undefined`) ? window.innerWidth : null,
+        height: (typeof window !== `undefined`) ? window.innerHeight : null,
+    });
 
     useEffect(() => {
         const handleResize = () => {
-            SetWindowSize(getSize());
+            SetWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
         }
 
         window.addEventListener('resize', handleResize);
